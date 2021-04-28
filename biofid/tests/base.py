e42b09c603522ebe573dc17575b8530f0f549f73
Adrian Pachzelt <a.pachzelt@gmail.com>
2021-04-28 12:20:31 +0200

    Added not_raises() test function; Set maxDiff = None in NoDatabaseTestCase

--- biofid/tests/base.py
+++ biofid/tests/base.py
@@ -1,13 +1,12 @@
-from time import sleep
-
+from contextlib import contextmanager
+from django.core.handlers.wsgi import WSGIRequest
 from django.test import LiveServerTestCase
+from django.test import RequestFactory
 from django.test import SimpleTestCase
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from django.test import RequestFactory
+from time import sleep
 from typing import Any
-from django.core.handlers.wsgi import WSGIRequest
-
+from webdriver_manager.chrome import ChromeDriverManager
 
 AJAX_REQUEST_HEADER = {'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'}
 
@@ -19,10 +18,16 @@ class AjaxRequestFactory(RequestFactory):
 
 
 class NoDatabaseTestCase(SimpleTestCase):
-    """ A test class that disables any databases involved and hence runs faster. """
-    databases = []
+    """ A test class that disables any databases involved and hence runs faster.
+        Removes the Diff size of some functions by setting maxDiff = None;
+        For details: https://docs.python.org/3/library/unittest.html#unittest.TestCase.maxDiff
+    """
+    databases = []  # No databases are loaded => Faster
+    maxDiff = None
 
-    def setUp(self) -> None:
+    def __init__(self, methodName: str = 'runTest'):
+        super().__init__(methodName)
+
         self.request_factory = RequestFactory()
         self.ajax_request_factory = AjaxRequestFactory()
 
@@ -60,3 +65,16 @@ class BiofidLiveWebsiteTestCase(LiveServerTestCase):
 
     def wait(self, waiting_time_in_seconds: int):
         sleep(waiting_time_in_seconds)
+
+
+@contextmanager
+def not_raises(exception):
+    """ Makes the intention clear that a function is run for the purpose of not raising a given exception.
+        Usage:
+            with not_raises(IndexException):
+                function_that_should_not_raise()
+    """
+    try:
+        yield
+    except exception:
+        raise ValueError("DID RAISE {0}".format(exception))
