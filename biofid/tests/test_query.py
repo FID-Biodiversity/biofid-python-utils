e0d00299d1e60f96d1d9667a5fca5bbaa9f8031f
Adrian Pachzelt <a.pachzelt@ub.uni-frankfurt.de>
2021-11-08 21:05:03 +0100

    Added solr escape function

---
+++ biofid/tests/test_query.py
@@ -0,0 +1,16 @@
+from biofid.tests.base import NoDatabaseTestCase
+from biofid.data import query
+
+
+class TestQuery(NoDatabaseTestCase):
+    def test_solr_escaping(self):
+        test_cases = [
+            ('foo bar', 'foo bar'),
+            ('foo || bar', 'foo \|\| bar'),
+            ('foo ? bar', 'foo \? bar'),
+            ('(1+1):2', '\(1\+1\)\:2')
+        ]
+
+        for solr_input, expected_result in test_cases:
+            with self.subTest(solr_input):
+                self.assertEquals(expected_result, query.escape_solr_input(solr_input))
